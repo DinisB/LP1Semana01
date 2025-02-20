3f9cea428312578673705681437e6c85eca80b9e
DinisB <dinisbarroso.dev@gmail.com>
2025-02-20 20:28:32 +0000

    Inteiros

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; 
+            sbyte a = -128;
+            byte b = 255;
+            short c = 32767;
+            ushort d = 65535;
+            int e = 2147483647;
+            uint f = 4294967295;
+            long g = 9223372036854775807;
+            ulong h = 18446744073709551615;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+        }
+    }
+}
