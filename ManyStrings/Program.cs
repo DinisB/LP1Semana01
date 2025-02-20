39144e40b08a735ab75fe422752f2dc6bed1bb01
DinisB <dinisbarroso.dev@gmail.com>
2025-02-20 20:54:09 +0000

    Strings normais

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; 
+            string str1 = "Bom dia";
+            string nome = "Jubileu";
+            char cruz = '\u2020';
+            Console.WriteLine(str1);
+            Console.WriteLine(nome + " morreu " + cruz);
+        }
+    }
+}
